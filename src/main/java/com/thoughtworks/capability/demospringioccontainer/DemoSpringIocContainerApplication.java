58b7e2e7404abc80548782868dfa6c7ff101507d
dai-yu <1435480344@qq.com>
2020-11-08 11:27:22 +0800

    [daiyu] testing successfully

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/DemoSpringIocContainerApplication.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/DemoSpringIocContainerApplication.java
@@ -2,12 +2,20 @@ package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoSpringIocContainerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringIocContainerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(DemoSpringIocContainerApplication.class, args);
+
+        GreetingController greetingController1 = context.getBean(GreetingController.class);
+        greetingController1.greet();
+
+        GreetingController greetingController2 = context.getBean(GreetingController.class);
+        greetingController2.greet();
+
+    }
 
 }
